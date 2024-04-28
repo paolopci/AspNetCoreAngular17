685a4594c472c5440a7d8cfd7b018ba2e2d3792b
PaoloPci <paolopci@yahoo.it>
2024-04-28 11:00:35 +0200

    add MatTableModule

--- worldcities.client/src/app/angular-material.module.ts
+++ worldcities.client/src/app/angular-material.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import{MatTableModule} from "@angular/material/table"
 
 
 @NgModule({
     imports:[
         MatButtonModule,
         MatIconModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatTableModule
     ],
     exports:[
         MatButtonModule,
         MatIconModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatTableModule
     ]
 })
 export class AngularMaterialModule{}
