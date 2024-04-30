78cfc2a064913cdc21beaa88ff3d2a1539872756
PaoloPci <paolopci@yahoo.it>
2024-04-30 17:22:12 +0200

    Form reactive client side create city-edit.component

--- worldcities.client/src/app/cities/city-edit.component.ts
+++ worldcities.client/src/app/cities/city-edit.component.ts
@@ -15,9 +15,53 @@ import { City } from './city';
 })
 export class CityEditComponent implements OnInit {
 
-  
+  title?: string;
+  form!: FormGroup;
+  city?: City;
+
+  constructor(private http: HttpClient, private activedRoute: ActivatedRoute, private router: Router) { }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.form = new FormGroup({
+      name: new FormControl(''),
+      lat: new FormControl(''),
+      lon: new FormControl('')
+    });
+    this.loadData();
   }
 
+
+  loadData() {
+    // retrieve the ID from the id parameter
+    var idParam = this.activedRoute.snapshot.paramMap.get('id');
+    var id = idParam ? idParam : 0;
+    // ritorno la città dal Server
+    var url = environment.baseUrl + 'api/Cities/' + id;
+    this.http.get<City>(url).subscribe({
+      next: (result) => {
+        this.city = result;
+        this.title = "Edit - " + this.city.name;
+        // update the form with the city value
+        this.form.patchValue(this.city);
+      }, error: (error) => console.error(error)
+    });
+  }
+
+  onSubmit() {
+    var city = this.city;
+    if (city) {
+      city.name = this.form.controls['name'].value;
+      city.lat = this.form.controls['lat'].value;
+      city.lon = this.form.controls['lon'].value;
+
+      var url = environment.baseUrl + 'api/Cities/' + city.id;
+      this.http.put<City>(url, city).subscribe({
+        next: (result) => {
+          console.log("City " + city!.id + " has been updated");
+          // go back to cities list
+          this.router.navigate(['/cities']);
+        }, error: (error) => console.error(error)
+      });
+    }
+  }
 }
