044451909513285ecfc744872e3d1a544c70aa0d
PaoloPci <paolopci@yahoo.it>
2024-04-30 10:12:21 +0200

    CountriesControllers server side and use [JsonPropertyName("<nome>")]

--- WorldCities.Server/Data/Models/Country.cs
+++ WorldCities.Server/Data/Models/Country.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -25,10 +26,12 @@ namespace WorldCities.Server.Data.Models
         /// <summary>
         /// Country code (in ISO 3166-1 ALPHA-2 format)
         /// </summary>
+        [JsonPropertyName("iso2")]
         public required string ISO2 { get; set; }
         /// <summary>
         /// Country code (in ISO 3166-1 ALPHA-3 format)
         /// </summary>
+         [JsonPropertyName("iso3")]
         public required string ISO3 { get; set; }
         #endregion
 
