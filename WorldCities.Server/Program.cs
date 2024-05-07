865f786aec89ca7c3de0ce9c2451fabc7929a4f9
PaoloPci <paolopci@yahoo.it>
2024-05-07 16:57:47 +0200

    setting Serilog

--- WorldCities.Server/Program.cs
+++ WorldCities.Server/Program.cs
@@ -1,10 +1,28 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
 using WorldCities.Server.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+
+// Add Serilog support
+//builder.Host.UseSerilog((ctx, lc) => lc
+//    .ReadFrom.Configuration(ctx.Configuration)
+//    .WriteTo.MSSqlServer(connectionString: ctx.Configuration.GetConnectionString("DefaultConnection"),
+//        restrictedToMinimumLevel: LogEventLevel.Information, 
+//        sinkOptions: new MSSqlServerSinkOptions
+//        {
+//            TableName = "LogEvents",
+//            AutoCreateSqlDatabase = true
+//        }).WriteTo.Console());
+
+
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -22,6 +40,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 
 var app = builder.Build();
+
+// Serilog Logging HTTP requests
+//app.UseSerilogRequestLogging();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
