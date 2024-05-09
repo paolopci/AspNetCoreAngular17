172546d9b493882b5d219bb0affc960e8044d520
PaoloPci <paolopci@yahoo.it>
2024-05-09 17:21:25 +0200

    fix LoginRequest

--- WorldCities.Server/Data/ApiLoginRequest.cs
+++ WorldCities.Server/Data/ApiLoginRequest.cs
@@ -7,7 +7,7 @@ namespace WorldCities.Server.Data
     public class ApiLoginRequest
     {
         [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Email is required.")]
-        public required string email { get; set; }
+        public required string Email { get; set; }
         [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Password is required.")]
         public required string Password { get; set; }
     }
